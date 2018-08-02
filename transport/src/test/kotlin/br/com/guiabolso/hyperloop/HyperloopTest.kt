46844ad643342127af337ba6263eda3aa8d48862
Bruno Ortiz <brunortiz11@gmail.com>
2018-08-02 13:59:28 -0300

    fixing tests

--- transport/src/test/kotlin/br/com/guiabolso/hyperloop/HyperloopTest.kt
+++ transport/src/test/kotlin/br/com/guiabolso/hyperloop/HyperloopTest.kt
@@ -3,10 +3,13 @@ package br.com.guiabolso.hyperloop
 import br.com.guiabolso.events.builder.EventBuilder
 import br.com.guiabolso.events.model.RequestEvent
 import br.com.guiabolso.hyperloop.cryptography.cypher.MessageCypher
+import br.com.guiabolso.hyperloop.exceptions.InvalidInputException
 import br.com.guiabolso.hyperloop.exceptions.SendMessageException
 import br.com.guiabolso.hyperloop.transport.MessageResult
 import br.com.guiabolso.hyperloop.transport.Transport
+import br.com.guiabolso.hyperloop.validation.ValidationResult
 import br.com.guiabolso.hyperloop.validation.Validator
+import br.com.guiabolso.hyperloop.validation.exceptions.ValidationException
 import com.github.salomonbrys.kotson.jsonObject
 import com.google.gson.Gson
 import com.nhaarman.mockitokotlin2.any
@@ -50,6 +53,7 @@ class HyperloopTest {
     @Test
     fun `test can send event as message`() {
         whenever(transport.sendMessage(any())).thenReturn(MessageResult("some-id", eventMD5))
+        whenever(validator.validate(any())).thenReturn(ValidationResult(true, mutableListOf(), mutableListOf()))
         whenever(cryptographyEngine.cypher(any())).thenAnswer {
             it.arguments[0] as String
         }
@@ -63,6 +67,18 @@ class HyperloopTest {
     @Test(expected = SendMessageException::class)
     fun `test send event fails with invalid md5`() {
         whenever(transport.sendMessage(any())).thenReturn(MessageResult("some-id", "wrong-md5-hash"))
+        whenever(validator.validate(any())).thenReturn(ValidationResult(true, mutableListOf(), mutableListOf()))
+        whenever(cryptographyEngine.cypher(any())).thenAnswer {
+            it.arguments[0] as String
+        }
+
+        hyperloop.offer(event)
+    }
+
+    @Test(expected = ValidationException::class)
+    fun `test validate event fails`() {
+        whenever(transport.sendMessage(any())).thenReturn(MessageResult("some-id", "wrong-md5-hash"))
+        whenever(validator.validate(any())).thenReturn(ValidationResult(false, mutableListOf(InvalidInputException("some input was invalid")), mutableListOf()))
         whenever(cryptographyEngine.cypher(any())).thenAnswer {
             it.arguments[0] as String
         }
